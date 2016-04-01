ac5c9076af7ab5fd2447b1d93ccf946472239c2a
steve <semmerich@keywcorp.com>
2016-04-01 08:31:10 -0600

    fixed icons on settings. finally

--- client/views/settings/settings.controller.js
+++ client/views/settings/settings.controller.js
@@ -1,9 +1,21 @@
 'use strict';
 
 angular.module('hog')
-  .controller('SettingsCtrl', function ($mdToast, $log, Settings) {
+  .controller('SettingsCtrl', function ($mdToast, $log, Settings, lodash) {
 
     var vm = this;
+    var priv = {
+      removedArgs: [],
+      getToastPosition: function()
+      {
+        return {
+          bottom: false,
+          top: true,
+          left: false,
+          right: true
+        };
+      }
+    }
     angular.extend(vm, {
       name: 'SettingsCtrl',
       ins: Settings.list(),
@@ -58,18 +70,62 @@ angular.module('hog')
         $mdToast.show(
           $mdToast.simple()
             .textContent('Error: ' + err +'!')
-            .position({
-              bottom: false,
-              top: true,
-              left: false,
-              right: true
-            })
+            .position("top right")
             .hideDelay(3000)
         )
       },
-      udfs: {displayName: 'User Defined Functions (UDFS):', data: []},
+      showRemovedArgToast: function(opt)
+      {
+        var pinTo = priv.getToastPosition();
+        var toast = $mdToast.simple()
+          .textContent('Removed as Pig Argument default')
+          .action('UNDO')
+          .highlightAction(true)
+          //.highlightClass('md-accent')// Accent is used by default, this just demonstrates the usage.
+          .position("top right");
+        $mdToast.show(toast)
+          .then(
+            function(response)
+            {
+              if ( response == 'ok' )
+              {
+                vm.ins[opt].data.push(priv.removedArgs.pop());
+                alert('You clicked the \'UNDO\' action.');
+              }
+            });
+      },
+      udfs: {displayName: 'User Defined Functions (UDF):', data: []},
+      removeArg: function(opt, index)
+      {
+        if(!lodash.isUndefined(vm.ins[opt].data[index]))
+        {
+          
+          if(index == vm.ins[opt].data.length-1)
+          {
+            priv.removedArgs.push(vm.ins[opt].data.pop());
+          }
+          else if(index == 0)
+          {
+            priv.removedArgs.push(vm.ins[opt].data.unshift());
+          }
+          else
+          {
+            priv.removedArgs.push(lodash.slice(vm.ins[opt].data,index));
+          }
+          vm.showRemovedArgToast(opt);
+         // delete vm.ins[opt].data[index];
+          $log.info("type of: ", Array.isArray(vm.ins[opt].data), "removed index: ", index, 'from: ', vm.ins[opt].data, priv.removedArgs);
+        }
+        
+        $log.info('remove clicked');
+      },
+      addArg: function(opt, temp)
+      {
+        vm.ins[opt].data.push({description: "", arg: temp, default: "" });
+        $log.info('add clicked');
+      }
     });
-  vm.showSavedToast();
+ 
   Settings.list()
     .then(
       function(data)
