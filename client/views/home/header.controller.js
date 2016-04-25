8d869ae2f3e58586775217a827c94c7bbc7fc4de
Kevin <kcoxe@keywcorp.com>
2016-04-25 08:36:47 -0400

    add pig-flow to bower

--- client/views/home/header.controller.js
+++ client/views/home/header.controller.js
@@ -6,7 +6,7 @@ angular.module('hog')
         $mdSidenav('left').close();
     }
     var vm = this;
-    
+
     $scope.$on('stateChangeSuccess',
         function()
         {
@@ -52,7 +52,7 @@ angular.module('hog')
               }
             ]
           },
-         
+
         ],
         toggleNav: function()
         {
