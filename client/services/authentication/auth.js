57db2b267baf1a349339d4584e42596da955ea29
steve <semmerich@keywcorp.com>
2016-05-17 17:44:55 -0400

    added socket connections for auth. maybe should just be http request though

--- client/services/authentication/auth.js
+++ client/services/authentication/auth.js
@@ -2,7 +2,7 @@
 
 angular.module('hog')
     .service('Auth',
-        function (socketFactory, $q, $sessionStorage)
+        function (socketFactory, $q, $window, $rootScope)
         {
             var auth = socketFactory()
             auth.forward('error');
@@ -22,16 +22,78 @@ angular.module('hog')
                 var defer = $q.defer();
                 //Log the user in
                 auth.ioSocket.emit('authenticate', {user: username, pass: password});
+                auth.ioSocket.on('authn',
+                  function(data)
+                  {
+                    if(data.status == 200)
+                    {
+                      // Do session token stuff
+                      $window.sessionStorage.token = data.token
+                      $rootScope.$broadcast('authenticated', data.token);
+                      defer.resolve();
+                    }
+                    else if (data.status == 500)
+                    {
+                      // Do error stuff
+                      delete $window.sessionStorage.token;
+                      defer.reject(data.err);
+                    }
+                  });
                 return defer.promise();
               },
-              logout: function()
+              logout: function(uid)
               {
                 var defer = $q.defer();
+                auth.ioSocket.emit('unauthenticate', {uid: uid});
+                auth.ioSocket.on('unauthn',
+                  function(data)
+                  {
+                    if(data.status == 200)
+                    {
+                      delete $window.sessionStorage.token;
+                      $rootScope.$broadcast('unauthenticated', null);
+                      // Do session token stuff
+                      defer.resolve();
+                    }
+                    else if (data.status == 500)
+                    {
+                      // Do error stuff
+                      $rootScope.$broadcast('unauthenticated', null);
+                      delete $window.sessionStorage.token;
+                      defer.reject(data.err);
+                    }
+                  });
                 // Log the user out
+              },
+              isAuthIntersepter: function()
+              {
+                return {
+                  request: function (config)
+                  {
+                    config.headers = config.headers || {};
+                    if ($window.sessionStorage.token)
+                    {
+                      config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+                    }
+                    return config;
+                  },
+                  response: function (response)
+                  {
+                    if (response.status === 401)
+                    {
+                      // handle the case where the user is not authenticated
+                    }
+                    return response || $q.when(response);
+                  }
+                }
               },
               isAuth: function()
               {
                 var defer = $q.defer();
+                if ($window.sessionStorage.token)
+                {
+                  defer.resolve();
+                }
                 // Is the user authenticated
                 return defer.promise();
               },
