7ec885c445a67164ff59b0f548da3c51be872536
steve <semmerich@keywcorp.com>
2016-06-01 17:39:57 -0400

    [:WIP:] [ci skip] Pig is working fine with the new Camo db ODM. Settings however are not loading for some reason. I have also added the versioning into the Pig schema so it will auto update the version on update.

--- server/api/pig/pig.model.js
+++ server/api/pig/pig.model.js
@@ -6,7 +6,113 @@ var spawn     = require('child_process').spawn;
 var logger    = require('../../config/logger.js');
 var path      = require('path');
 var ds        = require('nedb');
+var connect   = require('camo').connect;
+var Document  = require('camo').Document;
 
+class Pig extends Document {
+  constructor()
+  {
+    super();
+    
+    this.name       = String;
+    this.data       = String;
+    this.args       = [];
+    this.version    = String;
+    this.script_loc = String;
+  }
+  static collectionName() {
+        return 'pig.data';
+    }
+  run(stdoutCB, stderrCB, stdlogCB, stdwarningCB, finishedCB)
+  {
+    var script_location = '';
+    if (this.script_loc)
+    {
+      script_location = this.script_loc;
+    }
+    else
+    {
+      script_location = path.join(__dirname, '../../',  'scripts/pig/', this.name +  '.pig');
+    }
+
+    pigParser.run(this.args, script_location, stdoutCB, stderrCB, stdlogCB, stdwarningCB, finishedCB);
+  }
+  runAndTrack(stdoutCB, stderrCB, stdlogCB, stdwarningCB, finishedCB)
+  {
+    var script_location = '';
+    if (this.script_loc)
+    {
+      script_location = this.script_loc;
+    }
+    else
+    {
+      script_location = path.join(__dirname, '../../',  'scripts/pig/', this.name +  '.pig');
+    }
+
+    pigParser.trackTasks(this.args, script_location, stdoutCB, stderrCB, stdlogCB, stdwarningCB, finishedCB);
+  }
+  bumpMajor()
+  {
+    return parseFloat(this.version.split('.')[0]) + 1;
+  }
+  bumpMinor()
+  {
+    var v = this.version.split('.');
+    var vv = parseFloat(v[1]);
+    if(vv + 1 >= 10)
+    {
+      return 0;
+    }
+    else
+    {
+      return vv + 1;
+    }
+  }
+  bumpRelease()
+  {
+    var v = this.version.split('.');
+    var vv = parseFloat(v[2]);
+    if(vv + 1 >= 10)
+    {
+      return 0;
+    }
+    else
+    {
+      return vv + 1;
+    }
+  }
+  bump()
+  {
+    var v = this.version.split('.');
+    var release = this.bumpRelease();
+    var minor = v[1];
+    var major = v[0];
+    if (release == 0)
+    {
+      minor = this.bumpMinor();
+      if(minor == 0)
+      {
+        major = this.bumpMajor();
+      }
+    }
+    this.version = ''.concat(major,'.',minor,'.',release);
+  }
+  preSave()
+  {
+    this.bump();
+  }
+}
+
+exports.Pig = Pig;
+exports.init = function(cb)
+{
+  var uri = 'nedb://' + path.join(__dirname);
+  connect(uri).then(function(db) {
+    logger.info('connected to DB', db);
+    cb(null, db);
+  })
+}
+/*
 var collection = new ds({filename: 'server/api/pig/pig.data.db', autoload: true, onload: function (err) { if(err) { logger.error('Error on load: ', err) }}});
 
 exports.create = function(obj, cb)
@@ -121,3 +227,4 @@ exports.runAndTrack = function(id, stdoutCB, stderrCB, stdlogCB, stdwarningCB, t
     });
 
 };
+*/
